3a175e66d352a077f395ef3ddd0565b0a3cb9156
89605502155 <ferubko1999@yandex.ru>
2022-07-31 16:22:45 +0300

    We will work with only angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,28 +1,11 @@
-import { AfterViewInit, Component, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
-import  React from 'react';
-import  ReactDOM from 'react-dom';
-import { News } from './MyReactComponent';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
-  template: '<div [id]="rootId"></div>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnChanges, AfterViewInit, OnDestroy{
+export class AppComponent {
   title = 'frontend';
-  public rootId = 'rootId';
-  ngOnChanges(changes: SimpleChanges): void {
-    this.render()
-  }
-  ngAfterViewInit(): void {
-    this.render()
-  }
-  ngOnDestroy(): void {
-    
-  }
-
-  private render(){
-    ReactDOM.render(React.createElement(News),document.getElementById(this.rootId))
-  }
 }
---
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+
+})
+export class NavigationBar {
+  showMenu = false;
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
+  }
+}
