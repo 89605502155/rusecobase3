1048bec2919dfd37e2fd2f0eded7931c640d6bd8
89605502155 <ferubko1999@yandex.ru>
2022-07-31 11:00:58 +0300

    React work with Angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import  React from 'react';
+import  ReactDOM from 'react-dom';
+import { News } from './MyReactComponent';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<div [id]="rootId"></div>',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, AfterViewInit, OnDestroy{
   title = 'frontend';
+  public rootId = 'rootId';
+  ngOnChanges(changes: SimpleChanges): void {
+    this.render()
+  }
+  ngAfterViewInit(): void {
+    this.render()
+  }
+  ngOnDestroy(): void {
+    
+  }
+
+  private render(){
+    ReactDOM.render(React.createElement(News),document.getElementById(this.rootId))
+  }
 }
